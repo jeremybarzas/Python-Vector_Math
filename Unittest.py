9251776c229e2da8bdd266287a6ad22bb8cb2758
Jeremy Barzas <jbarzas@gmail.com>
2017-03-23 12:52:29 -0500

    vector math libary and unittest

---
+++ Unittest.py
@@ -0,0 +1,34 @@
+'''unittesting for vector math'''
+
+from Vector2 import Vector2
+
+def unit_test():
+    '''unit test'''
+    vec2a = Vector2([1, 1])
+    vec2b = Vector2([2, 2])
+
+    print "Addition"
+    add = vec2a.add(vec2b)
+    add.print_info()
+
+    print "\nSubtraction"
+    sub = vec2a.subtract(vec2b)
+    sub.print_info()
+
+    print "\nScalar Multiplication"
+    mult = vec2a.scalarmult(5)
+    mult.print_info()
+
+    print "\nMagnitude"
+    print vec2a.magnitude()
+
+    print "\nNormalize"
+    norm = vec2a.normalize()
+    norm.print_info()
+
+    print "\nDot Product"
+    print vec2a.dotproduct(vec2b)
+
+
+if __name__ == "__main__":
+    unit_test()
